7c80df9a52f0fadef47ce4e434d0678d60f76337
John Malley <john@teeturtle.com>
2020-03-10 15:59:30 -0600

    html for cart dropdown, promo bar, brand bar

---
+++ js/scripts.js
@@ -0,0 +1,75 @@
+// Cart dropdown JS
+$(document).ready(function () {
+
+    $('.cart-dropdown').click(function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+    });
+
+    // Increment using +/- buttons
+    $('.btn-number').click(function (e) {
+        e.preventDefault();
+
+        var fieldName = $(this).attr('data-field');
+        var type = $(this).attr('data-type');
+        var input = $('input[name="' + fieldName + '"]');
+        var currentVal = parseInt(input.val());
+        if (!isNaN(currentVal)) {
+            if (type == 'minus') {
+                if (currentVal > input.attr('min')) {
+                    input.val(currentVal - 1).change();
+                }
+                if (parseInt(input.val()) == input.attr('min')) {
+                    $(this).attr('disabled', true);
+                }
+
+            } else if (type == 'plus') {
+                input.val(currentVal + 1).change();
+            }
+        } else {
+            input.val(0);
+        }
+    });
+    $('.input-number').focusin(function () {
+        $(this).data('oldValue', $(this).val());
+    });
+    $('.input-number').change(function () {
+
+        var minValue = parseInt($(this).attr('min'));
+        var valueCurrent = parseInt($(this).val());
+
+        var name = $(this).attr('name');
+        if (valueCurrent >= minValue) {
+            $('.btn-number[data-type="minus"][data-field="' + name + '"]').removeAttr('disabled');
+        } else {
+            // TODO: Remove from cart when value reaches 0
+            alert('Minimum value was reached');
+            $(this).val($(this).data('oldValue'));
+        }
+
+
+    });
+
+    $('.input-number').keydown(function (e) {
+        // Allow backspace, delete, tab, escape, enter, home, end, left, right
+        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 35, 36, 37, 39]) !== -1 ||
+            // Allow Ctrl+A
+            (e.keyCode == 65 && e.ctrlKey === true)) {
+            return;
+        }
+        // Increment using up/down arrows
+        if (e.keyCode == 38) {
+            $(this).val(parseInt($(this).val()) + 1).change();
+        }
+        if (e.keyCode == 40) {
+            e.preventDefault();
+            $(this).val(parseInt($(this).val()) - 1).change();
+        }
+        // Prevent any other input other than numbers
+        if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+            e.preventDefault();
+        }
+    });
+
+    
+});
